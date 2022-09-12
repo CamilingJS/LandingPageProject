3903ef0d1b6e2e0c61c7c5d8e44451842c85d251
JayCam.Dev <jonathancamiling@gmail.com>
2022-09-12 06:45:39 +0000

    Update 5 files

---
+++ src/Components/ServiceBlock.js
@@ -0,0 +1,8 @@
+export default function ServiceBlock () {
+    
+    return (
+        <>
+            Hello from Service Block
+        </>
+    )
+}
--- src/Components/ServicesSection.js
+++ src/Components/ServicesSection.js
@@ -1,9 +1,11 @@
-import React from "react";
+import ServiceBlock from "./ServiceBlock";
 
 export default function ServicesSection() {
   return (
     <div className="servicesSection">
       <h1>Hello from Services Section</h1>
+      
+      <ServiceBlock />
     </div>
   );
 }
